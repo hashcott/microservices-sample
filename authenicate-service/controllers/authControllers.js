4e77776edecbac85f07f7cf7fc6328cbbf8cfb0c
hanh <duchanhstyle@gmail.com>
2021-02-17 16:32:45 +0700

    create User model

---
+++ authenicate-service/controllers/authControllers.js
@@ -0,0 +1,2 @@
+const User = require("../models/User");
+const jwt = require('jsonwebtoken');
--- authenicate-service/routes/authRoutes.js
+++ authenicate-service/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.post("/", (req , res) => res.jsonp({ msg : "REGISTRY" }))
-router.post("/auth" , (req, res) => res.jsonp({ msg : "AUTH" }))
+router.post("/registry", (req , res) => res.jsonp({ msg : "REGISTRY" }))
+router.post("/login" , (req, res) => res.jsonp({ msg : "AUTH" }))
 
 module.exports = router;
