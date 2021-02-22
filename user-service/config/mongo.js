979998c03c7c46254811ffb6230d856e50c7e403
hanh <duchanhstyle@gmail.com>
2021-02-23 00:13:44 +0700

    fix database name

--- user-service/config/mongo.js
+++ user-service/config/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports.MongoDB = async () => {
     try {
-        await mongoose.connect("mongodb://mongodb:27017/authenticate-service", {
+        await mongoose.connect("mongodb://mongodb:27017/user-service", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
